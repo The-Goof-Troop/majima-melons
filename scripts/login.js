3f808efe832e9b7ad9c8920b6c48ef19d69770db
Miles Weber <milesweber13@gmail.com>
2022-12-14 10:33:52 -0500

    Fixed login links

--- scripts/login.js
+++ scripts/login.js
@@ -3,11 +3,11 @@ let username = document.getElementById("username").value;
 let password = document.getElementById("password").value;
 if ( username == "user" && password == "password"){
 alert ("Login successfully");
-location.assign("https://the-goof-troop.github.io/majima-melons/index.html")
+location.assign("../index.html")
 }
 if ( username == "manager" && password == "manager_password"){
     alert ("Login successfully");
-    location.assign("http://127.0.0.1:5500/pages/manager.html")
+    location.assign("../pages/manager.html")
     }
     else{
         alert ("wrong credentials");
